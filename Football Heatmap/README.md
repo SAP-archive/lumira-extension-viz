edcbc28096035b59a7cf4597b6316d16a89ab3ad
Annie (Suantak Niangneihoi) <annie_st16@yahoo.com>
2015-06-30 15:54:57 -0700

    Football heatmap extension using SAP Web IDE

---
+++ Football Heatmap/README.md
@@ -0,0 +1,13 @@
+Football Heatmap - SAP Lumira visualization extension
+=================================================
+ * A custom heatmap chart extension for a football field.
+
+SAP Lumira files
+-----------
+* `sap.viz.ext.footballheatmap.zip` - SAP Lumira visualization extension, rebuilt in Web IDE
+
+
+//Resources
+-----------
+* Blog post - [Football Heatmap](http://scn.sap.com/community/lumira/blog/2015/06/30/football-heatmap-extension-for-sap-lumira)
+
---
+++ Football Heatmap/src/bundles/sap/viz/ext/footballheatmap/sap_viz_ext_footballheatmap-src/js/flow.js
@@ -0,0 +1,77 @@
+define("sap_viz_ext_footballheatmap-src/js/flow", [ "sap_viz_ext_footballheatmap-src/js/module" ], function(moduleFunc) {
+    var flowRegisterFunc = function(){
+        var flow = sap.viz.extapi.Flow.createFlow({
+            id : "sap.viz.ext.footballheatmap",
+            name : "Football Heatmap",
+            dataModel : "sap.viz.api.data.CrosstableDataset",
+            type : "DIV"
+        });
+
+        var titleElement  = sap.viz.extapi.Flow.createElement({
+            id : "sap.viz.chart.elements.Title",
+            name : "Title"
+        });
+        flow.addElement({
+            "element":titleElement,
+            "propertyCategory":"title",
+            "place":"top"
+        });
+
+        var legendElement  = sap.viz.extapi.Flow.createElement({
+            id : "sap.viz.chart.elements.ColorLegend",
+            name : "Legend",
+            dimensionIndex: [1]
+        });
+        flow.addElement({
+            "element":legendElement,
+            "propertyCategory":"legend",
+            "place":"right"
+        });
+        
+        var element  = sap.viz.extapi.Flow.createElement({
+            id : "sap.viz.ext.footballheatmap.PlotModule",
+            name : "Football Heatmap Module"
+        });
+        element.implement("sap.viz.elements.common.BaseGraphic", moduleFunc);
+
+        /*Feeds Definition*/
+        var ds1 = {
+            "id": "sap.viz.ext.footballheatmap.PlotModule.DS1",
+            "name": "Untitled Dimension Set 1",
+            "type": "Dimension",
+            "min": 0, //minimum number of data container
+            "max": 2, //maximum number of data container
+            "aaIndex": 1
+        };
+        element.addFeed(ds1);
+        
+        var ms1 = {
+            "id": "sap.viz.ext.footballheatmap.PlotModule.MS1",
+            "name": "Untitled Measure Set 1",
+            "type": "Measure",
+            "min": 0, //minimum number of measures
+            "max": Infinity, //maximum number of measures
+            "mgIndex": 1
+        };
+        element.addFeed(ms1);
+        
+
+        element.addProperty({
+            name: "colorPalette",
+            type: "StringArray",
+            supportedValues: "",
+            defaultValue: d3.scale.category20().range().concat(d3.scale.category20b().range()).concat(d3.scale.category20c().range())
+        });
+
+        flow.addElement({
+            "element":element,
+            "propertyCategory" : "plotArea"
+        });
+        sap.viz.extapi.Flow.registerFlow(flow);
+    };
+    flowRegisterFunc.id = "sap.viz.ext.footballheatmap";
+    return {
+        id : flowRegisterFunc.id,
+        init : flowRegisterFunc
+    };
+});
