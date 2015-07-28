0d6da21c1b37b6631c224486dc1dd9327efbfbdd
Annie (Suantak Niangneihoi) <annie_st16@yahoo.com>
2015-07-27 18:20:54 -0700

    Sankey Diagram based on Google Visualization API

---
+++ Google_API_Sankey_Diagram/bundles/pan/viz/ext/sankey/pan_viz_ext_sankey-src/resources/templates/standard/template.js
@@ -0,0 +1,11 @@
+var sampleTemplate = 
+{
+    "id": "standard",
+    "name": "Standard",
+    "properties": {
+        "pan.viz.ext.sankey": {
+            
+        }
+    }
+};
+sap.viz.extapi.env.Template.register(sampleTemplate);
