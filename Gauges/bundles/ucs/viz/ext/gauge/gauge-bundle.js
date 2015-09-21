b7c795422f94bd670b6e7ab58dbb4167ecb9c030
Annie (Suantak Niangneihoi) <annie_st16@yahoo.com>
2015-09-21 14:33:07 -0700

    Added: D3 Gauges by Andrew Jabbitt

---
+++ Gauges/bundles/ucs/viz/ext/gauge/gauge-bundle.js
@@ -0,0 +1,47 @@
+define("gauge-bundle", ["ucs_viz_ext_gauge-src/js/flow", "css!ucs_viz_ext_gauge-src/style/default.css"], function(flowDefinition, cssStyleDeclaration) {
+    var cssString = "", rules, i;
+    if (cssStyleDeclaration && cssStyleDeclaration.cssRules) {
+      rules = cssStyleDeclaration.cssRules;
+      for (i = 0; i < rules.length; i++) {
+        cssString += rules.item(i).cssText;
+      }
+    }
+    var vizExtImpl = {
+        viz   : [flowDefinition],
+        module: [],
+        feeds : [],
+        cssString : cssString
+    };
+    var vizExtBundle = sap.bi.framework.declareBundle({
+        "id" : "ucs.viz.ext.gauge",
+        "version" : "1.0.0",
+        "components" : [{
+            "id" : "ucs.viz.ext.gauge",
+            "provide" : "sap.viz.impls",
+            "instance" : vizExtImpl,
+            "customProperties" : {
+                "name" : "Gauge",
+                "description" : "",
+                "icon" : {"path" : ""},
+                "category" : [],
+                "requires": [{
+                    "id": "sap.viz.common.core",
+                    "version": "5.7.3"
+                }],
+                "resources" : [{"key":"sap.viz.api.env.Template.loadPaths", "path":"./ucs_viz_ext_gauge-src/resources/templates"}]
+            }
+        }]
+   });
+   // sap.bi.framework.getService is defined in BundleLoader, which is
+   // always available at this timeframe
+   // in standalone mode sap.viz.js will force load and active the
+   // "sap.viz.aio" bundle
+   if (sap.bi.framework.getService("sap.viz.aio", "sap.viz.extapi")) {
+       // if in standalone mode, sap.viz.loadBundle will be available,
+       // and we load the bundle directly
+       return sap.bi.framework.getService("sap.viz.aio", "sap.viz.extapi").core.registerBundle(vizExtBundle);
+   } else {
+       // if loaded by extension framework, return the "sap.viz.impls"
+       return vizExtBundle;
+   }
+});
