1fe58ff9ae109ad86ad5a9003c703bf264a4ab43
Annie (Suantak Niangneihoi) <annie_st16@yahoo.com>
2015-11-24 00:11:05 -0800

    Updated extension
    Added bug fixes

---
+++ Stacked_Column_with_Line_Chart/src/bundles/sap/viz/ext/stackedcolumnlinechart/sap_viz_ext_stackedcolumnlinechart-src/resources/templates/standard/template.js
@@ -0,0 +1,10 @@
+var sampleTemplate = {
+	"id": "standard",
+	"name": "Standard",
+	"properties": {
+		"sap.viz.ext.stackedcolumnlinechart": {
+
+		}
+	}
+};
+sap.viz.extapi.env.Template.register(sampleTemplate);
