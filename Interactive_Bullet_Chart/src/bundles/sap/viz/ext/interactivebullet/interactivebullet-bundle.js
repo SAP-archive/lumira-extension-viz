29995703e6377b26f7ab12b5f4a7eb7a7c7b0cf5
Annie (Suantak Niangneihoi) <annie_st16@yahoo.com>
2016-02-10 12:33:42 -0800

    Updated to version 1.0.2

--- Interactive_Bullet_Chart/src/bundles/sap/viz/ext/interactivebullet/interactivebullet-bundle.js
+++ Interactive_Bullet_Chart/src/bundles/sap/viz/ext/interactivebullet/interactivebullet-bundle.js
@@ -16,7 +16,7 @@ define("interactivebullet-bundle", ["sap_viz_ext_interactivebullet-src/js/flow",
 		};
 		var vizExtBundle = sap.bi.framework.declareBundle({
 			"id": "sap.viz.ext.interactivebullet",
-			"version": "1.0.1",
+			"version": "1.0.2",
 			"components": [{
 				"id": "sap.viz.ext.interactivebullet",
 				"provide": "sap.viz.impls",
