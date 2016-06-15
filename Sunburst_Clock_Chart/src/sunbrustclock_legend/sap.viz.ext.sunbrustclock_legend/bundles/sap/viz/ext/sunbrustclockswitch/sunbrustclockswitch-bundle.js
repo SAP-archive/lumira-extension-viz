c33c1776274e3b59334c462281067c4600088e75
Annie (Suantak Niangneihoi) <annie_st16@yahoo.com>
2016-06-15 14:13:09 -0700

    Republish Sunburst Clock Chart

---
+++ Sunburst_Clock_Chart/src/sunbrustclock_legend/sap.viz.ext.sunbrustclock_legend/bundles/sap/viz/ext/sunbrustclockswitch/sunbrustclockswitch-bundle.js
@@ -0,0 +1,55 @@
+define("sunbrustclockswitch-bundle", ["sap_viz_ext_sunbrustclockswitch-src/js/flow",
+	"css!sap_viz_ext_sunbrustclockswitch-src/style/default.css"
+], function(flowDefinition, cssStyleDeclaration) {
+	var cssString = "",
+		rules, i;
+	if (cssStyleDeclaration && cssStyleDeclaration.cssRules) {
+		rules = cssStyleDeclaration.cssRules;
+		for (i = 0; i < rules.length; i++) {
+			cssString += rules.item(i).cssText;
+		}
+	}
+	var vizExtImpl = {
+		viz: [flowDefinition],
+		module: [],
+		feeds: [],
+		cssString: cssString
+	};
+	var vizExtBundle = sap.bi.framework.declareBundle({
+		"id": "sap.viz.ext.sunbrustclockswitch",
+		"version": "1.0.1",
+		"components": [{
+			"id": "sap.viz.ext.sunbrustclockswitch",
+			"provide": "sap.viz.impls",
+			"instance": vizExtImpl,
+			"customProperties": {
+				"name": "SunbrustClock_switch",
+				"description": "",
+				"icon": {
+					"path": ""
+				},
+				"category": [],
+				"requires": [{
+					"id": "sap.viz.common.core",
+					"version": "5.6.0"
+				}],
+				"resources": [{
+					"key": "sap.viz.api.env.Template.loadPaths",
+					"path": "./sap_viz_ext_sunbrustclockswitch-src/resources/templates"
+				}]
+			}
+		}]
+	});
+	// sap.bi.framework.getService is defined in BundleLoader, which is
+	// always available at this timeframe
+	// in standalone mode sap.viz.js will force load and active the
+	// "sap.viz.aio" bundle
+	if (sap.bi.framework.getService("sap.viz.aio", "sap.viz.extapi")) {
+		// if in standalone mode, sap.viz.loadBundle will be available,
+		// and we load the bundle directly
+		return sap.bi.framework.getService("sap.viz.aio", "sap.viz.extapi").core.registerBundle(vizExtBundle);
+	} else {
+		// if loaded by extension framework, return the "sap.viz.impls"
+		return vizExtBundle;
+	}
+});
